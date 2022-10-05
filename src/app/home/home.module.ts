f7d8a9f0bd80424a987839b820fcbec2a3b8c49d
Moulaïli Saïdi <91431031+Moulayli@users.noreply.github.com>
2022-10-05 10:54:35 +0200

    tinymce editor

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { EditorModule } from '@tinymce/tinymce-angular';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    EditorModule 
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements  OnInit {
+  tinyMCEConfiguration;
+  constructor() {
+    this.tinyMCEConfiguration ={
+      base_url: '/tinymce', 
+      suffix: '.min'       
+    }
+  }
 
-  constructor() {}
-
+  ngOnInit() {
+    
+  }
 }
+  
+
