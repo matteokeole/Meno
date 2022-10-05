44fc948c0e8712b8391e0fef2194465ef0601e83
Mattéo <matteokeole@icloud.com>
2022-10-05 11:50:44 +0200

    TinyMCE debounced save

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,17 +6,30 @@ import { Component ,OnInit  } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements  OnInit {
-  tinyMCEConfiguration;
+  private editorConfig: object;
+  private timeout;
+  private delay: number;
+
   constructor() {
-    this.tinyMCEConfiguration ={
-      base_url: '/tinymce', 
-      suffix: '.min'       
+    this.editorConfig = {
+      base_url: "/tinymce", 
+      suffix: ".min",       
     }
+
+    this.delay = 250;
   }
 
-  ngOnInit() {
-    
+  ngOnInit(): void {}
+
+  init(e): void {
+    // Hide the TinyMCE status bar
+    document.querySelector(".tox-statusbar").remove();
+  }
+
+  save(e): void {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      console.log("Saving...");
+    }, this.delay);
   }
 }
-  
-
