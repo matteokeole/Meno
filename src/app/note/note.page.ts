b3833983821734a882fe3254fe3ea4d321544c2c
Moulaïli Saïdi <91431031+Moulayli@users.noreply.github.com>
2022-10-06 17:19:16 +0200

    tinymce styled

--- src/app/note/note.page.ts
+++ src/app/note/note.page.ts
@@ -3,6 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 import { IonRouterOutlet, ModalController } from '@ionic/angular';
 import { Note, NoteService } from '../note.service';
 import { DetailPage } from '../detail/detail.page';
+import { PluginListenerHandle } from '@capacitor/core';
+import { Motion  } from '@capacitor/motion';
+
+
+
 
 @Component({
   selector: 'app-note',
@@ -18,69 +23,94 @@ export class NotePage implements OnInit {
     private noteService: NoteService,
     private modalController: ModalController,
     private activatedRoute: ActivatedRoute,
-    private routerOutlet: IonRouterOutlet,
+    private routerOutlet: IonRouterOutlet
   ) {
     // TinyMCE configuration
     this.editorConfig = {
-      base_url: "/tinymce",
-      suffix: ".min",
-      height: 800,
-    }
+      base_url: '/tinymce',
+      suffix: '.min',
+      height: '95vh',
+      menubar: true,
+      toolbar_location: 'bottom',
+      toolbar:
+        'undo redo |  strikethrough | styleselect | formatselect | bold italic backcolor | forecolor |\
+      bullist numlist ',
+      content_style:
+        'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+    };
   }
 
   ngOnInit() {
     // Retrieve the note with the route ID
-    const id = +this.activatedRoute.snapshot.paramMap.get("id");
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
     this.note = this.noteService.find(id);
   }
 
   /**
    * Initializes the TinyMCE editor for the note.
-   * 
+   *
    * @param {object} e  Event
    */
   init(e) {
-    const {editor} = e;
+    const { editor } = e;
 
     // Set note content
     editor.setContent(this.note.content);
 
     // Hide the TinyMCE status bar
-    document.querySelector(".tox-statusbar").remove();
+    document.querySelector('.tox-statusbar').remove();
   }
 
   /**
    * Saves the TinyMCE editor content.
    * 250ms debounce.
-   * 
+   *
    * @param {object} e  Event
    */
   save(e) {
-    const {editor} = e;
+    const { editor } = e;
 
     // Reset the timeout
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
       this.note.content = editor.getContent();
+
       this.noteService.save(this.note);
+      console.warn(editor.getContent());
     }, 250);
   }
 
   /**
    * Displays the note details modal.
-   * 
+   *
    * @see {@link ../detail/detail.page.ts}
    * @async
    */
   async showDetails() {
-    (await this.modalController.create({
-      component: DetailPage,
-      componentProps: {
-        note: this.note,
-      },
-      canDismiss: true,
-      breakpoints: [.25, .5, .75],
-      initialBreakpoint: .25,
-    })).present();
+    (
+      await this.modalController.create({
+        component: DetailPage,
+        componentProps: {
+          note: this.note,
+        },
+        canDismiss: true,
+        presentingElement: this.routerOutlet.nativeEl,
+      })
+    ).present();
   }
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
 }
