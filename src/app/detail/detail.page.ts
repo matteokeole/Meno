679070847d4759517a1b074212ae9c655bc544a5
Mattéo <matteokeole@icloud.com>
2022-10-06 16:51:57 +0200

    Added note title modification

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { AlertController, NavController, ToastController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 import { Note, NoteService } from '../note.service';
-import * as Utils  from '../utils' 
+import * as Utils from '../utils';
 
 @Component({
   selector: 'app-detail',
@@ -16,14 +15,30 @@ export class DetailPage implements OnInit {
 
   constructor(
     private noteService: NoteService,
-    private alertController: AlertController,
     private toastController: ToastController,
-    private navController: NavController,
-    private router: Router,
   ) {}
 
   ngOnInit() {
     this.updatedAt = new Intl.DateTimeFormat("fr-FR").format(this.note.updatedAt);
+  }
+
+  /**
+   * Changes the note title.
+   * A toast is displayed upon success.
+   * 
+   * @async
+   * @param e
+   */
+  async changeTitle(e) {
+    this.note.title = e.target.value;
+    this.noteService.save(this.note);
+
+    (await this.toastController.create({
+      icon: "checkmark-circle-outline",
+      message: "Le titre a été mis à jour.",
+      color: "success",
+      duration: 2000,
+    })).present();
   }
 
   toggleFavorite() {
